61fc24173507d520ea5fb9cfa5dde71666670992
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-25 16:03:17 -0400

    add random tips

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -10,17 +10,17 @@ print("╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\n")
 print("*_Bem vindo ao jogo de Adivinhação_*\n")
 print("************************************\n")
 
-numero_secreto = int(random.random() * 100)
+numero_secreto = random.randrange(1, 101)
 tentativas = 3
 
 print("Dica, o número está entre {} e {}".format(
-    numero_secreto - 15, numero_secreto + 15))
+    numero_secreto - random.randrange(1, 30), numero_secreto + random.randrange(1, 30)))
 
 for i in range(1, tentativas + 1):
 
     if (tentativas == 1):
         print("Dica, o número está entre {} e {}".format(
-            numero_secreto - 3, numero_secreto + 3))
+            numero_secreto - random.randrange(1, 3), numero_secreto + random.randrange(1, 3)))
 
     tentativas -= 1
 
@@ -28,13 +28,14 @@ for i in range(1, tentativas + 1):
     print("Você digitou", chute)
 
     if (numero_secreto == chute):
-        print("\nVocê acertou\n")
+        print("\n+ + + Você acertou + + +\n")
         break
     elif (numero_secreto < chute):
-        print("Você errou, seu chute foi acima")
+        print("- - -  Você errou, seu chute foi acima - - - ")
         print(f"\n{tentativas} Tentativa(s) restante(s)\n")
     else:
-        print("Você errou, seu chute foi abaixo")
+        print("- - - Você errou, seu chute foi abaixo - - - ")
         print(f"\n{tentativas} Tentativa(s) restante(s)\n")
 
-print("\nFim de Jogo\n")
+print("\nFim de Jogo")
+print(f"O número secreto era {numero_secreto}")
