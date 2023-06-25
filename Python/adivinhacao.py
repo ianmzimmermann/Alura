b1c79e0eea9bb4e5fdd8beef114f477b085e3229
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-25 14:44:18 -0400

    more cond - loop

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -9,12 +9,21 @@ print("*_Bem vindo ao jogo de Adivinhação_*\n")
 print("************************************\n")
 
 numero_secreto = 67
+tentativas = 3
 
-chute = int(input("Digite o seu numero:"))
+while (tentativas > 0):
 
-print("Você digitou", chute)
+    tentativas -= 1
+    chute = int(input("Digite o seu numero:"))
+    print("Você digitou", chute)
 
-if (numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou")
+    if (numero_secreto == chute):
+        print("Você acertou")
+    elif (numero_secreto < chute):
+        print("Você errou, seu chute foi acima")
+        print(tentativas, "Tentativa(s) restante(s)", "\n")
+    else:
+        print("Você errou, seu chute foi abaixo")
+        print(tentativas, "Tentativa(s) restante(s)", "\n")\
+
+print("\n\nFim de Jogo\n\n")
