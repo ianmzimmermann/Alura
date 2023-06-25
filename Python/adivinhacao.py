399bd47e0d03a10a4b13aa44b13ed482d93e4218
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-25 16:34:22 -0400

    organized - add score

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -10,12 +10,14 @@ print("╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\n")
 print("*_Bem vindo ao jogo de Adivinhação_*\n")
 print("************************************\n")
 
-numero_secreto = random.randrange(1, 101)
-
 print("Selecione o nível de dificuldade")
 print("3 Fácil \n2 Médio \n1 Difícil")
 
 tentativas = 3 * int(input("Digite o nível:"))
+
+pontos = 100
+
+numero_secreto = random.randrange(1, 101)
 
 print("Dica, o número está entre {} e {}".format(
     numero_secreto - random.randrange(1, 30), numero_secreto + random.randrange(1, 30)))
@@ -32,14 +34,19 @@ for i in range(1, tentativas + 1):
     print("Você digitou", chute)
 
     if (numero_secreto == chute):
-        print("\n+ + + Você acertou + + +\n")
+        print("\n+ + + Você acertou + + +")
         break
     elif (numero_secreto < chute):
         print("- - -  Você errou, seu chute foi acima - - - ")
         print(f"\n{tentativas} Tentativa(s) restante(s)\n")
+        pontos -= abs(numero_secreto/10)
     else:
         print("- - - Você errou, seu chute foi abaixo - - - ")
         print(f"\n{tentativas} Tentativa(s) restante(s)\n")
+        pontos -= abs(numero_secreto/10)
+
+pontos = abs(pontos)
 
 print("\nFim de Jogo")
+print(f"Total de pontos {pontos}\n")
 print(f"O número secreto era {numero_secreto}")
