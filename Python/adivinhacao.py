440a7c82873f4932f2ac6cd73c3f80cdebe872ca
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-25 11:47:32 -0400

    iniciando jogo

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -1,1 +1,20 @@
-print("Bem vindo ao jogo de Adivinhação")
+print("┏━━━┓\t┏━━━┓\t┏━━━┓\t┏━━━┓\t┏━━━┓")
+print("┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃")
+print("┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃")
+print("╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛")
+print("╋╋┏┓\t╋╋┏┓\t╋╋┏┓\t╋╋┏┓\t╋╋┏┓")
+print("╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\n")
+
+print("*_Bem vindo ao jogo de Adivinhação_*\n")
+print("************************************\n")
+
+numero_secreto = 67
+
+chute = int(input("Digite o seu numero:"))
+
+print("Você digitou", chute)
+
+if (numero_secreto == chute):
+    print("Você acertou")
+else:
+    print("Você errou")
