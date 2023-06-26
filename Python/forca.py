3e6243c196a1756e759f3c9a7c44cfbd1f0ecef6
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-26 15:35:59 -0400

    add func to game

--- Python/forca.py
+++ Python/forca.py
@@ -1,7 +1,24 @@
 def jogar():
-
     print("*_Bem vindo ao jogo de Forca_*\n")
     print("************************************\n")
+
+    palavra_secreta = "python"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ").lower().strip()
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print(letra, end=' ')
+            else:
+                print("_", end=' ')
+            index += 1
+
+        print("\n")
 
     print("\nFim de Jogo")
 
