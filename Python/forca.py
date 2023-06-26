a12f72dc89e27007f8b6231dc9445396a1ed21da
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-26 19:05:31 -0400

    add trys - stop condition

--- Python/forca.py
+++ Python/forca.py
@@ -10,21 +10,34 @@ def jogar():
 
     enforcou = False
     acertou = False
+    tentativas = 6
 
     while (not enforcou and not acertou):
         chute = input("Qual letra? ").lower().strip()
         index = 0
 
-        for letra in palavra_secreta:
-            if (chute == letra):
-                letras_acertadas[index] = letra
-                print("Acertou")
-            index += 1
+        if (chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                    print("Acertou")
+                index += 1
+        else:
+            tentativas -= 1
+            print(f"{tentativas} Tentativas restantes\n")
+
+        enforcou = tentativas == 0
+        acertou = "_" not in letras_acertadas
 
         print(letras_acertadas)
         print("\n")
 
-    print("\nFim de Jogo")
+    if (acertou):
+        print("Parabéns, você descobriu\n")
+    else:
+        print("Você perdeu, que tal tentar outra vez?\n")
+
+    print("Fim de Jogo")
 
 
 if (__name__ == "__main__"):
