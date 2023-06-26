ceb982e91b5081efdb05999e4c0574bbc4f97171
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-26 13:46:53 -0400

    add def

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -1,50 +1,53 @@
 import random
 
-print("┏━━━┓\t┏━━━┓\t┏━━━┓\t┏━━━┓\t┏━━━┓")
-print("┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃")
-print("┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃")
-print("╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛")
-print("╋╋┏┓\t╋╋┏┓\t╋╋┏┓\t╋╋┏┓\t╋╋┏┓")
-print("╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\n")
 
-print("*_Bem vindo ao jogo de Adivinhação_*\n")
-print("************************************\n")
+def jogo_adivinhacao():
 
-print("Selecione o nível de dificuldade")
-print("3 Fácil \n2 Médio \n1 Difícil")
+    print("┏━━━┓\t┏━━━┓\t┏━━━┓\t┏━━━┓\t┏━━━┓")
+    print("┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃\t┃┏━┓┃")
+    print("┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃\t┗┛┏┛┃")
+    print("╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛\t╋╋┃┏┛")
+    print("╋╋┏┓\t╋╋┏┓\t╋╋┏┓\t╋╋┏┓\t╋╋┏┓")
+    print("╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\t╋╋┗┛\n")
 
-tentativas = 3 * int(input("Digite o nível:"))
+    print("*_Bem vindo ao jogo de Adivinhação_*\n")
+    print("************************************\n")
 
-pontos = 1000
+    print("Selecione o nível de dificuldade")
+    print("3 Fácil \n2 Médio \n1 Difícil")
 
-numero_secreto = random.randrange(1, 101)
+    tentativas = 3 * int(input("Digite o nível:"))
 
-print("Dica, o número está entre {} e {}".format(
-    numero_secreto - random.randrange(1, 30), numero_secreto + random.randrange(1, 30)))
+    pontos = 1000
 
-for i in range(1, tentativas + 1):
+    numero_secreto = random.randrange(1, 101)
 
-    if (tentativas == 1):
-        print("Dica, o número está entre {} e {}".format(
-            numero_secreto - random.randrange(1, 3), numero_secreto + random.randrange(1, 3)))
+    print("Dica, o número está entre {} e {}".format(
+        numero_secreto - random.randrange(1, 30), numero_secreto + random.randrange(1, 30)))
 
-    tentativas -= 1
+    for i in range(1, tentativas + 1):
 
-    chute = int(input("Digite o seu numero:"))
-    print("Você digitou", chute)
+        if (tentativas == 1):
+            print("Dica, o número está entre {} e {}".format(
+                numero_secreto - random.randrange(1, 3), numero_secreto + random.randrange(1, 3)))
 
-    if (numero_secreto == chute):
-        print("\n+ + + Você acertou + + +")
-        break
-    elif (numero_secreto < chute):
-        print("- - -  Você errou, seu chute foi acima - - - ")
-        print(f"\n{tentativas} Tentativa(s) restante(s)\n")
-        pontos = abs(pontos - numero_secreto)
-    else:
-        print("- - - Você errou, seu chute foi abaixo - - - ")
-        print(f"\n{tentativas} Tentativa(s) restante(s)\n")
-        pontos = abs(pontos - numero_secreto)
+        tentativas -= 1
 
-print("\nFim de Jogo")
-print(f"Total de pontos {pontos}\n")
-print(f"O número secreto era {numero_secreto}")
+        chute = int(input("Digite o seu numero:"))
+        print("Você digitou", chute)
+
+        if (numero_secreto == chute):
+            print("\n+ + + Você acertou + + +")
+            break
+        elif (numero_secreto < chute):
+            print("- - -  Você errou, seu chute foi acima - - - ")
+            print(f"\n{tentativas} Tentativa(s) restante(s)\n")
+            pontos = abs(pontos - numero_secreto)
+        else:
+            print("- - - Você errou, seu chute foi abaixo - - - ")
+            print(f"\n{tentativas} Tentativa(s) restante(s)\n")
+            pontos = abs(pontos - numero_secreto)
+
+    print("\nFim de Jogo")
+    print(f"Total de pontos {pontos}\n")
+    print(f"O número secreto era {numero_secreto}")
