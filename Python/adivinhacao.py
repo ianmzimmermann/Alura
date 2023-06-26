6011ee33070b63d4e0a4c040d7f0713c282dfdcc
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-26 13:33:32 -0400

    changed score

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -15,7 +15,7 @@ print("3 Fácil \n2 Médio \n1 Difícil")
 
 tentativas = 3 * int(input("Digite o nível:"))
 
-pontos = 100
+pontos = 1000
 
 numero_secreto = random.randrange(1, 101)
 
@@ -39,13 +39,11 @@ for i in range(1, tentativas + 1):
     elif (numero_secreto < chute):
         print("- - -  Você errou, seu chute foi acima - - - ")
         print(f"\n{tentativas} Tentativa(s) restante(s)\n")
-        pontos -= abs(numero_secreto/10)
+        pontos = abs(pontos - numero_secreto)
     else:
         print("- - - Você errou, seu chute foi abaixo - - - ")
         print(f"\n{tentativas} Tentativa(s) restante(s)\n")
-        pontos -= abs(numero_secreto/10)
-
-pontos = abs(pontos)
+        pontos = abs(pontos - numero_secreto)
 
 print("\nFim de Jogo")
 print(f"Total de pontos {pontos}\n")
