ab15f9698f43a2a994f60d2dfa3eee4bcd0fc252
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-27 10:39:20 -0400

    creat extrato

--- Python/Orientado Objeto/conta.py
+++ Python/Orientado Objeto/conta.py
@@ -6,3 +6,6 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
