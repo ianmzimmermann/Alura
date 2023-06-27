34396107cad5a5c470d04b51afb5339cfcaab448
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-27 08:25:08 -0400

    organized

--- Python/forca.py
+++ Python/forca.py
@@ -1,14 +1,8 @@
 import random
 
 
-def inicializando(palavra):
-
-    return ["_" for i in palavra]
-
-
 def jogar():
-
-    init()
+    abertura()
 
     palavra_secreta = palavra()
 
@@ -20,43 +14,19 @@ def jogar():
     acertou = False
     tentativas = 6
 
-    while (not enforcou and not acertou):
-        chute = input("Qual letra? ").lower().strip()
-        index = 0
+    acertou = jogando(palavra_secreta, letras_acertadas,
+                      enforcou, acertou, tentativas)
 
-        if (chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                    print("Acertou")
-                index += 1
-        else:
-            tentativas -= 1
-            print(f"{tentativas} Tentativas restantes\n")
-
-        enforcou = tentativas == 0
-        acertou = "_" not in letras_acertadas
-
-        print(letras_acertadas)
-        print("\n")
-
-    if (acertou):
-        print("Parabéns, você descobriu\n")
-    else:
-        print("Você perdeu, que tal tentar outra vez?\n")
-
-    print("Fim de Jogo")
+    print_final(acertou)
 
 
-def init():
-
+def abertura():
     print("************************************\n", end="")
     print("****_Bem vindo ao jogo de Forca_****\n", end="")
     print("************************************\n")
 
 
 def palavra():
-
     palavras = []
 
     with open("palavras.txt") as arquivo:
@@ -66,6 +36,46 @@ def palavra():
     palavra_secreta = palavras[random.randrange(0, len(palavras))]
 
     return palavra_secreta
+
+
+def inicializando(palavra):
+    return ["_" for i in palavra]
+
+
+def chute_correto(palavra_secreta, letras_acertadas, chute):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+            print("Acertou")
+        index += 1
+
+
+def jogando(palavra_secreta, letras_acertadas, enforcou, acertou, tentativas):
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ").lower().strip()
+
+        if (chute in palavra_secreta):
+            chute_correto(palavra_secreta, letras_acertadas, chute)
+        else:
+            tentativas -= 1
+            print(f"{tentativas} Tentativas restantes\n")
+
+        enforcou = tentativas == 0
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+        print("\n")
+    return acertou
+
+
+def print_final(acertou):
+    if (acertou):
+        print("Parabéns, você descobriu\n")
+    else:
+        print("Você perdeu, que tal tentar outra vez?\n")
+
+    print("Fim de Jogo")
 
 
 if (__name__ == "__main__"):
