33db96bba680f2fa2bf5c1e72f4e5467103c2935
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-27 16:27:51 -0400

    modelos

---
+++ Python/Orientado Objeto/modelos.py
@@ -0,0 +1,30 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
+      f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2018, 2)
+
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
+      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
