f450decbb79bf84bd7cbf6c8f10fd916e2f2b9ff
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-28 10:59:17 -0400

    slicing url

--- Python/Extrator_url/main.py
+++ Python/Extrator_url/main.py
@@ -1,2 +1,9 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
+
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:len(url)]
+print(url_parametros)
