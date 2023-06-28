caff5fbe04c5027c8b9d31d92b561f7471a7787a
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-28 11:04:35 -0400

    dynamic slicing

--- Python/Extrator_url/main.py
+++ Python/Extrator_url/main.py
@@ -1,9 +1,10 @@
-url = "bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
 
+indice_base = url.find('?')
 
-url_base = url[0:19]
+url_base = url[0:indice_base]
 print(url_base)
 
-url_parametros = url[20:len(url)]
+url_parametros = url[indice_base + 1:len(url)]
 print(url_parametros)
