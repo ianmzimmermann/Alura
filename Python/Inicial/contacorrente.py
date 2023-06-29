0e4a489f7d7d3a1a26f61b81bcc56ba6fc061461
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-29 17:34:34 -0400

    polymorphism

--- Python/Inicial/contacorrente.py
+++ Python/Inicial/contacorrente.py
@@ -9,3 +9,38 @@ class Conta:
 
     def __str__(self):
         return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+
+class ContaCorrente(Conta):
+
+    def passa_mes(self):
+        self._saldo -= 2
+
+
+class ContaPoupanca(Conta):
+
+    def passa_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
+
+
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta16.passa_mes()
+print(conta16)
+
+conta17 = ContaPoupanca(17)
+conta17.deposita(1000)
+conta17.passa_mes()
+print(conta17)
+
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta17 = ContaPoupanca(17)
+conta17.deposita(1000)
+
+
+contas = [conta16, conta17]
+for conta in contas:
+    conta.passa_mes()
+    print(conta)
