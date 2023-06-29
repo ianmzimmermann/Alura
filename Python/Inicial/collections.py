f0f3e0fbbe6c2390b31a56431447f4250a32c194
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-29 16:14:03 -0400

    list

---
+++ Python/Inicial/collections.py
@@ -0,0 +1,29 @@
+idade1 = 39
+idade2 = 76
+idade3 = 15
+idade4 = 21
+
+print(idade1)
+print(idade2)
+print(idade3)
+print(idade4)
+
+
+idades = [39, 76, 15, 21]
+type(idades)
+
+print(type(idades))
+print(idades)
+print(idades[3])
+
+idades.append(87)
+
+print(idades)
+
+idades.remove(76)
+
+print(idades)
+
+idades.clear()
+
+print(idades)
