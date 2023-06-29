72a2cf21ed60c0be7dcc6dea24daabce23f7ca7f
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-29 16:36:02 -0400

    list operations

--- Python/Inicial/collections.py
+++ Python/Inicial/collections.py
@@ -8,7 +8,6 @@ print(idade2)
 print(idade3)
 print(idade4)
 
-
 idades = [39, 76, 15, 21]
 type(idades)
 
@@ -27,3 +26,22 @@ print(idades)
 idades.clear()
 
 print(idades)
+
+idades = [39, 76, 15, 21]
+
+idades.insert(1, 87)
+
+idades.extend([11, 25])
+
+print(idades)
+
+for idade in idades:
+    print(idade + 1)
+
+idades_prox_ano = []
+for idade in idades:
+    idades_prox_ano.append(idade + 1)
+
+print(idades_prox_ano)
+
+print([(idade + 1)for idade in idades])
