ad1ac9c2fb6052090ef74b1caf24a07344040ca6
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-29 13:39:28 -0400

    checking empty url - removing unnecessary param

--- Python/Extrator_url/main.py
+++ Python/Extrator_url/main.py
@@ -1,19 +1,23 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-# print(url)
+# url = " "
+
+if url.strip() == "":
+    raise ValueError("Url está vazia")
 
 indice_base = url.find('?')
 
-url_base = url[0:indice_base]
+url_base = url[:indice_base]
 # print(url_base)
 
 # if (indice_base):
-url_parametros = url[indice_base + 1:len(url)]
+url_parametros = url[indice_base + 1:]
 print(url_parametros)
 
 param_busca = 'moedaOrigem'
 indice_param = url_parametros.find(param_busca)
 indice_moeda = indice_param + len(param_busca) + 1
 indice_divisao = url_parametros.find('&', indice_moeda)
+
 if (indice_divisao == -1):
     moeda_origem = url_parametros[indice_moeda:]
 else:
