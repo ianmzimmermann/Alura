535c3e8cd120d8955b4e63b40e409c0ee565f72b
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-06-29 17:28:29 -0400

    basic struct

---
+++ Python/Inicial/contacorrente.py
@@ -0,0 +1,11 @@
+class Conta:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
