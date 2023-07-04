85f064060dfb157730c6d62bef59ea8f2f74aeac
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-07-04 16:25:56 -0400

    new test sobrenome

--- Python/TDD/test/test_bytebank.py
+++ Python/TDD/test/test_bytebank.py
@@ -2,12 +2,22 @@ from codigo.bytebank import Funcionario
 
 
 class TestClass:
-    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+    def test_quando_idade_recebe_13_03_2000_retornar_22(self):
         entrada = '13/03/2000'  # Given-Contexto
         esperado = 23
 
         funcionario_teste = Funcionario('Teste', entrada, 1111)
 
         resultado = funcionario_teste.idade()  # When-ação
+
+        assert resultado == esperado  # Then-desfecho
+
+    def test_quand0_sobrenome_recebe_Ian_Zimmermann_retornar_Zimmermann(self):
+        entrada = 'Ian Zimmermann'  # Given-Contexto
+        esperado = 'Zimmermann'
+
+        funcionario_teste = Funcionario(entrada, 25, 1111)
+
+        resultado = funcionario_teste.sobrenome()  # When-ação
 
         assert resultado == esperado  # Then-desfecho
