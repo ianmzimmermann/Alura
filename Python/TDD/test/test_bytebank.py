017c1879b36bd669bfd9bd0c96a224942cd0b844
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-07-04 17:12:37 -0400

    new test salario

--- Python/TDD/test/test_bytebank.py
+++ Python/TDD/test/test_bytebank.py
@@ -21,3 +21,15 @@ class TestClass:
         resultado = funcionario_teste.sobrenome()  # When-ação
 
         assert resultado == esperado  # Then-desfecho
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000  # Given-Contexto
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+
+        funcionario_teste.decrescimo_salario()  # When-ação
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado  # Then-desfecho
