cea86debe7c2778b5c2a87f123628f3ce6cb1899
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-07-04 14:32:04 -0400

    auto test

--- Python/TDD/code/main.py
+++ Python/TDD/code/main.py
@@ -1,5 +1,12 @@
 from bytebank import Funcionario
 
-ian = Funcionario('Ian', '19/03/1998', 6000)
+# ian = Funcionario('Ian', '19/03/1998', 6000)
+# print(ian.idade())
 
-print(ian.idade())
+
+def teste_idade():
+    funcionario_teste = Funcionario('Ian', '19/03/1998', 6000)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+
+teste_idade()
