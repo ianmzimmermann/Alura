8741995b3af6b1c419d52f5991f100ded8b03d24
Ian M Zimmermann <ian.m.zmm@gmail.com>
2023-07-04 15:33:12 -0400

    value correction

--- Python/TDD/test/test_bytebank.py
+++ Python/TDD/test/test_bytebank.py
@@ -4,7 +4,7 @@ from codigo.bytebank import Funcionario
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
         entrada = '13/03/2000'  # Given-Contexto
-        esperado = 22
+        esperado = 23
 
         funcionario_teste = Funcionario('Teste', entrada, 1111)
 
